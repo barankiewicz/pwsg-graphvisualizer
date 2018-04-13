d9a727ecb82b3f3aa28a8d810b4024526a9eac30
Filip Barankiewicz <barankiewicz@protonmail.ch>
2018-04-13 18:29:57 +0200

    Initial commit

---
+++ pwsg-graphvisualizer/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace WinFormslab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainWind = new System.Windows.Forms.PictureBox();
+            this.menuPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.editBox = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.colorShower = new System.Windows.Forms.Panel();
+            this.colorButton = new System.Windows.Forms.Button();
+            this.langBox = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.plLangButton = new System.Windows.Forms.Button();
+            this.engLangButton = new System.Windows.Forms.Button();
+            this.saveBox = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.importButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.mainWind)).BeginInit();
+            this.menuPanel.SuspendLayout();
+            this.editBox.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.langBox.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.saveBox.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainWind
+            // 
+            this.mainWind.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainWind.Location = new System.Drawing.Point(0, 0);
+            this.mainWind.Name = "mainWind";
+            this.mainWind.Size = new System.Drawing.Size(784, 562);
+            this.mainWind.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.mainWind.TabIndex = 1;
+            this.mainWind.TabStop = false;
+            this.mainWind.Click += new System.EventHandler(this.mainWind_Click);
+            this.mainWind.Paint += new System.Windows.Forms.PaintEventHandler(this.mainWind_Paint);
+            this.mainWind.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainWind_MouseDown);
+            // 
+            // menuPanel
+            // 
+            this.menuPanel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuPanel.ColumnCount = 1;
+            this.menuPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.menuPanel.Controls.Add(this.editBox, 0, 0);
+            this.menuPanel.Controls.Add(this.langBox, 0, 1);
+            this.menuPanel.Controls.Add(this.saveBox, 0, 2);
+            this.menuPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.menuPanel.Location = new System.Drawing.Point(584, 0);
+            this.menuPanel.Name = "menuPanel";
+            this.menuPanel.RowCount = 3;
+            this.menuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.menuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.menuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.menuPanel.Size = new System.Drawing.Size(200, 562);
+            this.menuPanel.TabIndex = 2;
+            // 
+            // editBox
+            // 
+            this.editBox.Controls.Add(this.tableLayoutPanel1);
+            this.editBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.editBox.Location = new System.Drawing.Point(3, 3);
+            this.editBox.Name = "editBox";
+            this.editBox.Size = new System.Drawing.Size(194, 316);
+            this.editBox.TabIndex = 0;
+            this.editBox.TabStop = false;
+            this.editBox.Text = "Edycja";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 67F));
+            this.tableLayoutPanel1.Controls.Add(this.colorShower, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.colorButton, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 16);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(188, 297);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // colorShower
+            // 
+            this.colorShower.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.colorShower.Location = new System.Drawing.Point(124, 3);
+            this.colorShower.Name = "colorShower";
+            this.colorShower.Size = new System.Drawing.Size(61, 24);
+            this.colorShower.TabIndex = 3;
+            // 
+            // colorButton
+            // 
+            this.colorButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.colorButton.Location = new System.Drawing.Point(3, 3);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(115, 24);
+            this.colorButton.TabIndex = 0;
+            this.colorButton.Text = "Kolor";
+            this.colorButton.UseVisualStyleBackColor = true;
+            this.colorButton.Click += new System.EventHandler(this.colorButton_Click);
+            // 
+            // langBox
+            // 
+            this.langBox.Controls.Add(this.tableLayoutPanel2);
+            this.langBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.langBox.Location = new System.Drawing.Point(3, 325);
+            this.langBox.Name = "langBox";
+            this.langBox.Size = new System.Drawing.Size(194, 114);
+            this.langBox.TabIndex = 1;
+            this.langBox.TabStop = false;
+            this.langBox.Text = "Język";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.plLangButton, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.engLangButton, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 16);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(188, 95);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // plLangButton
+            // 
+            this.plLangButton.AutoSize = true;
+            this.plLangButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plLangButton.Enabled = false;
+            this.plLangButton.Location = new System.Drawing.Point(3, 3);
+            this.plLangButton.Name = "plLangButton";
+            this.plLangButton.Size = new System.Drawing.Size(182, 41);
+            this.plLangButton.TabIndex = 0;
+            this.plLangButton.Text = "Polski";
+            this.plLangButton.UseVisualStyleBackColor = true;
+            // 
+            // engLangButton
+            // 
+            this.engLangButton.AutoSize = true;
+            this.engLangButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.engLangButton.Enabled = false;
+            this.engLangButton.Location = new System.Drawing.Point(3, 50);
+            this.engLangButton.Name = "engLangButton";
+            this.engLangButton.Size = new System.Drawing.Size(182, 42);
+            this.engLangButton.TabIndex = 1;
+            this.engLangButton.Text = "Angielski";
+            this.engLangButton.UseVisualStyleBackColor = true;
+            // 
+            // saveBox
+            // 
+            this.saveBox.Controls.Add(this.tableLayoutPanel3);
+            this.saveBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.saveBox.Location = new System.Drawing.Point(3, 445);
+            this.saveBox.Name = "saveBox";
+            this.saveBox.Size = new System.Drawing.Size(194, 114);
+            this.saveBox.TabIndex = 2;
+            this.saveBox.TabStop = false;
+            this.saveBox.Text = "Import/Eksport";
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.importButton, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.saveButton, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 16);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(188, 95);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // importButton
+            // 
+            this.importButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.importButton.Enabled = false;
+            this.importButton.Location = new System.Drawing.Point(3, 50);
+            this.importButton.Name = "importButton";
+            this.importButton.Size = new System.Drawing.Size(182, 42);
+            this.importButton.TabIndex = 2;
+            this.importButton.Text = "Import";
+            this.importButton.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.saveButton.Enabled = false;
+            this.saveButton.Location = new System.Drawing.Point(3, 3);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(182, 41);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Zapisz";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.menuPanel);
+            this.Controls.Add(this.mainWind);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Edytor Grafów";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.mainWind)).EndInit();
+            this.menuPanel.ResumeLayout(false);
+            this.editBox.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.langBox.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.saveBox.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox mainWind;
+        private System.Windows.Forms.TableLayoutPanel menuPanel;
+        private System.Windows.Forms.GroupBox editBox;
+        private System.Windows.Forms.GroupBox langBox;
+        private System.Windows.Forms.GroupBox saveBox;
+        private System.Windows.Forms.Button colorButton;
+        private System.Windows.Forms.Button engLangButton;
+        private System.Windows.Forms.Button plLangButton;
+        private System.Windows.Forms.Button importButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Panel colorShower;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+    }
+}
+
---
+++ pwsg-graphvisualizer/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormslab2
+{
+    public partial class Form1 : Form
+    {
+        Pen mainPen = new Pen(Color.Black, 3.5f);
+        SolidBrush drawBrush = new SolidBrush(Color.Black);
+        int vertexCount = 1;
+
+        List<Point> locations = new List<Point>();
+        List<PointF> locationsString = new List<PointF>();
+        List<Color> colors = new List<Color>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            menuPanel.Width = (int)(this.Width * 0.2);
+            mainWind.Width = (int)(this.Width * 0.8);
+            colorShower.BackColor = Color.Black;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            menuPanel.Width = (int)(this.Width * 0.2);
+            mainWind.Width = (int)(this.Width * 0.8);
+
+            
+            Graphics g = mainWind.CreateGraphics();
+            for (int i = 0; i < locations.Count; i++)
+            {
+                Pen pen = new Pen(colors[i], 3.5f);
+                SolidBrush brush = new SolidBrush(colors[i]);
+                g.DrawEllipse(pen, new Rectangle(locations[i], new Size(40, 40)));
+                g.DrawString((i+1).ToString(), new Font("Arial", 10), brush, locationsString[i]);
+            }
+        }
+
+        private void colorButton_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                colorShower.BackColor = colorDialog1.Color;
+                mainPen.Color = colorDialog1.Color;
+                drawBrush.Color = colorDialog1.Color;
+            }
+        }
+
+        private void mainWind_MouseDown(object sender, MouseEventArgs e)
+        {
+            //CREATE A VERTEX
+            var loc = e.Location;
+            foreach (Point location in locations)
+            {
+                int x = location.X;
+                int y = location.Y;
+                if (Math.Abs(loc.X - x) < 60 && Math.Abs(loc.Y - y) < 60)
+                    return;
+            }
+            Point circleLocation = new Point(e.Location.X - 20, e.Location.Y - 20);
+            PointF stringLocation = new PointF(e.Location.X - 5, e.Location.Y - 6);
+            locations.Add(circleLocation);
+            locationsString.Add(stringLocation);
+            colors.Add(drawBrush.Color);
+            Graphics g = mainWind.CreateGraphics();
+
+            g.DrawEllipse(mainPen, new Rectangle(circleLocation, new Size(40,40)));
+            g.DrawString(vertexCount.ToString(), new Font("Arial", 10), drawBrush, stringLocation);
+            vertexCount++;
+        }
+
+        private void mainWind_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mainWind_Paint(object sender, PaintEventArgs e)
+        {
+        }
+    }
+}
